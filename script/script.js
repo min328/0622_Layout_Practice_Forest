618625bc8e32c8d86aef4583e58b7e0cacc0f407
min328 <kmj20212021@gmail.com>
2021-06-22 15:13:42 +0900

    temporary jQuery Added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
 /* eslint-disable */
+
+
+
+    $(function () {
+       $("div.tabBtn").click(function(){
+           $(this).css({
+           "color" : "red"
+            , "border-bottom" : "2px solid red"
+        });
+           $(this).children("img").attr("src","images/tab_check.png")
+       });
+    });
+
